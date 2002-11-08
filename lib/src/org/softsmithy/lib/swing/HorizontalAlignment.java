6bb7c24da3d54718bdc36733fc6ea02e1e72dcb0
Florian Brunner <fbrunnerreg@gmx.ch>
2002-11-08 20:46:18 +0000

    Code cleaned

--- lib/src/org/softsmithy/lib/swing/HorizontalAlignment.java
+++ lib/src/org/softsmithy/lib/swing/HorizontalAlignment.java
@@ -19,7 +19,7 @@ import java.util.*;
 import javax.swing.*;
 import org.softsmithy.lib.util.*;
 
-public abstract class HorizontalAlignment extends TypesafeEnum implements Serializable{
+public abstract class HorizontalAlignment extends TypesafeEnum{
   
   private static final ResourceBundleCache cache = new ResourceBundleCache("org.softsmithy.lib.swing.HorizontalAlignment");
   
@@ -27,37 +27,56 @@ public abstract class HorizontalAlignment extends TypesafeEnum implements Serial
     super(s);
   }
   
-  //  private HorizontalAlignment(){ // should only be used by serialization
-  //    super("");
-  //  }
   
   public String toString(Locale locale){
     return cache.getBundle(locale).getString(toString());
-  }
-  
-  private Object writeReplace() throws ObjectStreamException{
-    return new Integer(getSwingConstant());
   }
   
   public abstract int getSwingConstant();
   public abstract String getHtmlConstant();
   
   
-  public static final HorizontalAlignment LEFT = new LeftHorizontalAlignment();
+  public static final HorizontalAlignment LEFT = new HorizontalAlignment("left"){
+    public int getSwingConstant(){
+      return SwingConstants.LEFT;
+    }
+    public String getHtmlConstant() {
+      return "left";
+    }
+  };
+  
   public static final HorizontalAlignment CENTER = new HorizontalAlignment("center"){
     public int getSwingConstant(){
       return SwingConstants.CENTER;
-    }
-    private Object readResolve() throws ObjectStreamException{
-      return HorizontalAlignment.CENTER;
     }
     public String getHtmlConstant() {
       return "center";
     }
   };
-  public static final HorizontalAlignment RIGHT = new RightHorizontalAlignment();
-  public static final HorizontalAlignment LEADING = new LeadingHorizontalAlignment();
-  public static final HorizontalAlignment TRAILING = new TrailingHorizontalAlignment();
+  public static final HorizontalAlignment RIGHT = new HorizontalAlignment("right"){    
+    public int getSwingConstant(){
+      return SwingConstants.RIGHT;
+    }
+    public String getHtmlConstant() {
+      return "right";
+    }
+  };
+  public static final HorizontalAlignment LEADING = new  HorizontalAlignment("leading"){    
+    public int getSwingConstant(){
+      return SwingConstants.LEADING;
+    }  
+    public String getHtmlConstant() {
+      return "left"; // no i18n
+    }
+  };
+  public static final HorizontalAlignment TRAILING = new HorizontalAlignment("trailing"){    
+    public int getSwingConstant(){
+      return SwingConstants.TRAILING;
+    }
+    public String getHtmlConstant() {
+      return "right"; // no i18n
+    }
+  };
   
   private static final HorizontalAlignment[] PRIVATE_VALUES = {LEFT, CENTER, RIGHT, LEADING, TRAILING};
   public static final List VALUES = Collections.unmodifiableList(Arrays.asList(PRIVATE_VALUES));
@@ -71,93 +90,5 @@ public abstract class HorizontalAlignment extends TypesafeEnum implements Serial
   public static HorizontalAlignment getHorizontalAlignment(int horizontalAlignment){
     return (HorizontalAlignment) alignments.get(new Integer(horizontalAlignment));
   }
-  
-  private static class LeftHorizontalAlignment extends HorizontalAlignment{
-    
-    public LeftHorizontalAlignment(){
-      super("left");
-    }
-    
-    public int getSwingConstant(){
-      return SwingConstants.LEFT;
-    }
-    private Object readResolve() throws ObjectStreamException{
-      return HorizontalAlignment.LEFT;
-    }
-    public String getHtmlConstant() {
-      return "left";
-    }
-  }
-  
-  private static class CenterHorizontalAlignment extends HorizontalAlignment{
-    
-    public CenterHorizontalAlignment(){
-      super("center");
-    }
-    
-    public int getSwingConstant(){
-      return SwingConstants.CENTER;
-    }
-    private Object readResolve() throws ObjectStreamException{
-      return HorizontalAlignment.CENTER;
-    }
-    public String getHtmlConstant() {
-      return "center";
-    }
-  }
-  
-  private static class RightHorizontalAlignment extends HorizontalAlignment{
-    
-    public RightHorizontalAlignment(){
-      super("right");
-    }
-    
-    public int getSwingConstant(){
-      return SwingConstants.RIGHT;
-    }
-    private Object readResolve() throws ObjectStreamException{
-      return HorizontalAlignment.RIGHT;
-    }
-    public String getHtmlConstant() {
-      return "right";
-    }
-  }
-  
-  private static class LeadingHorizontalAlignment extends HorizontalAlignment{
-    
-    public LeadingHorizontalAlignment(){
-      super("leading");
-    }
-    
-    public int getSwingConstant(){
-      return SwingConstants.LEADING;
-    }
-    
-    public String getHtmlConstant() {
-      return "left"; // no i18n
-    }
-    private Object readResolve() throws ObjectStreamException{
-      return HorizontalAlignment.LEADING;
-    }
-  }
-  
-  private static class TrailingHorizontalAlignment extends HorizontalAlignment{
-    
-    public TrailingHorizontalAlignment(){
-      super("trailing");
-    }
-    
-    public int getSwingConstant(){
-      return SwingConstants.TRAILING;
-    }
-    
-    private Object readResolve() throws ObjectStreamException{
-      return HorizontalAlignment.TRAILING;
-    }
-    
-    public String getHtmlConstant() {
-      return "right"; // no i18n
-    }
-    
-  }
+
 }
