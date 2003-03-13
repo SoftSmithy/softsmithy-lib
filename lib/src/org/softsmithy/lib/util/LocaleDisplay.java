ce8fc3fdc96fcc7374d5a98a4d1c67429e120670
Florian Brunner <fbrunnerreg@gmx.ch>
2003-03-13 18:48:12 +0000

    *** empty log message ***

--- lib/src/org/softsmithy/lib/util/LocaleDisplay.java
+++ lib/src/org/softsmithy/lib/util/LocaleDisplay.java
@@ -1,4 +1,18 @@
 /*
+ *                  Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is SoftSmithy Utility Library. The Initial Developer of the
+ * Original Code is Florian Brunner (Sourceforge.net user: puce). All Rights Reserved.
+ *
+ * Contributor(s): .
+ */
+
+/*
  * LocaleDisplay.java
  *
  * Created on 12. März 2003, 22:50
