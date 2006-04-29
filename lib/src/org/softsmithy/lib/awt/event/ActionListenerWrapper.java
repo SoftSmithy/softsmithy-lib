3860d8f4acf4c184fa5d61a37f2aa7ede9ca56bf
Florian Brunner <fbrunnerreg@gmx.ch>
2006-04-29 17:08:46 +0000

    Newline problem (windows <-> linux)

--- lib/src/org/softsmithy/lib/awt/event/ActionListenerWrapper.java
+++ lib/src/org/softsmithy/lib/awt/event/ActionListenerWrapper.java
@@ -12,17 +12,20 @@
  * Contributor(s): .
  */
 
+
+
 package org.softsmithy.lib.awt.event;
 
 import java.awt.event.*;
 
 public class ActionListenerWrapper implements ActionListener{
-  
+
   private ActionListener actionListener;
-  
+
   public ActionListenerWrapper(ActionListener actionListener){
     this.actionListener = actionListener;
   }
+
   public void actionPerformed(ActionEvent actionEvent){
     actionListener.actionPerformed(actionEvent);
   }
