48d01e2d05770d911355813dc906da32d6b524d9
Florian Brunner <fbrunnerreg@gmx.ch>
2006-06-18 13:09:06 +0000

    Moved properties-files from i18n to src directory.

--- lib/src/org/softsmithy/lib/swing/table/HorizontalAlignmentRenderer.java
+++ lib/src/org/softsmithy/lib/swing/table/HorizontalAlignmentRenderer.java
@@ -1,71 +1,142 @@
 /*
+
  *                  Sun Public License Notice
+
  *
+
  * The contents of this file are subject to the Sun Public License
+
  * Version 1.0 (the "License"); you may not use this file except in
+
  * compliance with the License. A copy of the License is available at
+
  * http://www.sun.com/
+
  *
+
  * The Original Code is SoftSmithy Utility Library. The Initial Developer of the
+
  * Original Code is Florian Brunner (Sourceforge.net user: puce). All Rights Reserved.
+
  *
+
  * Contributor(s): .
+
  */
 
+
+
 /*
+
  * HorizontalAlignmentCellRenderer.java
+
  *
+
  * Created on 7. Oktober 2002, 22:32
+
  */
+
+
 
 package org.softsmithy.lib.swing.table;
 
+
+
 import java.util.*;
+
 import javax.swing.table.*;
+
 import org.softsmithy.lib.swing.*;
 
 
+
+
+
 /**
+
  *
+
  * @author  puce
+
  */
+
 public class HorizontalAlignmentRenderer extends DefaultTableCellRenderer{
+
   
+
   /** Holds value of property locale. */
+
   private Locale locale;
+
   
+
   /** Creates a new instance of HorizontalAlignmentCellRenderer */
+
   public HorizontalAlignmentRenderer(Locale locale) {
+
     this.locale = locale;
+
   }
 
+
+
   /** Sets the <code>String</code> object for the cell being rendered to
+
    * <code>value</code>.
+
    *
+
    * @param value  the string value for this cell; if value is
+
    * 		<code>null</code> it sets the text value to an empty string
+
    * @see JLabel#setText
+
    *
+
    *
+
    */
+
   protected void setValue(Object value) {
+
     super.setValue(((HorizontalAlignment) value).toString(getLocale()));
+
   }  
+
   
+
   /** Getter for property locale.
+
    * @return Value of property locale.
+
    *
+
    */
+
   public Locale getLocale() {
+
     return this.locale;
+
   }
+
   
+
   /** Setter for property locale.
+
    * @param locale New value of property locale.
+
    *
+
    */
+
   public void setLocale(Locale locale) {
+
     this.locale = locale;
+
   }
+
   
+
 }
+
