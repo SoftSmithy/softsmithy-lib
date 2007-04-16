654fd4e83fa42416448563ac9e384a7f2e07fa28
Florian Brunner <fbrunnerreg@gmx.ch>
2007-04-16 11:55:27 +0000

    org.softsmithy.lib.util.Strings.cut added

--- lib/src/org/softsmithy/lib/util/Strings.java
+++ lib/src/org/softsmithy/lib/util/Strings.java
@@ -27,39 +27,52 @@ package org.softsmithy.lib.util;
  * @author  puce
  */
 public class Strings {
-  
-  /** Creates a new instance of Strings */
-  private Strings() {
-  }
-  
-  public static String join(String[] strings, String delimiter){
-    return join(strings, false, delimiter, false);
-  }
-  
-  public static String join(String[] strings, boolean prepend, String delimiter){
-    return join(strings, prepend, delimiter, false);
-  }
-  
-  public static String join(String[] strings, String delimiter, boolean append){
-    return join(strings, false, delimiter, append);
-  }
-  
-  public static String join(String[] strings, boolean prepend, String delimiter, boolean append){
-    StringBuffer string = new StringBuffer();
-    if (prepend){
-      string.append(delimiter);
+    
+    /** Creates a new instance of Strings */
+    private Strings() {
     }
-    for (int i=0; i<strings.length; i++){
-      string.append(strings[i]);
-      if (i < strings.length-1){
-        string.append(delimiter);
-      }
+    
+    public static String join(String[] strings, String delimiter){
+        return join(strings, false, delimiter, false);
     }
-    if (append){
-      string.append(delimiter);
+    
+    public static String join(String[] strings, boolean prepend, String delimiter){
+        return join(strings, prepend, delimiter, false);
     }
-    return string.toString();
-  }
-  
-  
+    
+    public static String join(String[] strings, String delimiter, boolean append){
+        return join(strings, false, delimiter, append);
+    }
+    
+    public static String join(String[] strings, boolean prepend, String delimiter, boolean append){
+        StringBuffer string = new StringBuffer();
+        if (prepend){
+            string.append(delimiter);
+        }
+        for (int i=0; i<strings.length; i++){
+            string.append(strings[i]);
+            if (i < strings.length-1){
+                string.append(delimiter);
+            }
+        }
+        if (append){
+            string.append(delimiter);
+        }
+        return string.toString();
+    }
+    
+    /**
+     * Cuts a string to the specified length.
+     * @param s the string to cut
+     * @param length the maximal length of the string
+     * @return if the string is smaller or equal to the specified length,
+     * the string will just be returned.
+     * Otherwise the first n characters of the string will be returned,
+     * where n is equal to the specified length.
+     */
+    // TODO good name? better 'substring'? only length parameter?
+    public static String cut(String s, int length){
+        return s.length() <= length ? s : s.substring(0, length);
+    }
+    
 }
