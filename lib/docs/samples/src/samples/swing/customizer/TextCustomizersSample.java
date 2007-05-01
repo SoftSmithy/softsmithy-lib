1bb1cced8b02628b63c8483372a003dded7a7be8
Florian Brunner <fbrunnerreg@gmx.ch>
2007-05-01 16:02:21 +0000

    JCheckBox and JRadioButton added to the TextCustomizersSample

--- lib/docs/samples/src/samples/swing/customizer/TextCustomizersSample.java
+++ lib/docs/samples/src/samples/swing/customizer/TextCustomizersSample.java
@@ -20,6 +20,9 @@
 
 package samples.swing.customizer;
 
+import java.awt.Color;
+import javax.swing.JCheckBox;
+import javax.swing.JRadioButton;
 import org.softsmithy.lib.awt.layout.AbsoluteTableConstraints;
 import org.softsmithy.lib.awt.layout.InfiniteTableLayout;
 import org.softsmithy.lib.swing.JButtonCustomizer;
@@ -43,10 +46,22 @@ public class TextCustomizersSample extends javax.swing.JFrame {
         JLabelCustomizer labelCustomizer = new JLabelCustomizer("A Label Customizer - double click to edit!");
         // add it to the JCustomizerPane
         customizerPane.addCustomizer(labelCustomizer, new AbsoluteTableConstraints(50, 50,  270, 20, labelCustomizer, itl));
-        // create a JButtonCustomizer, which supports inline editing of a text
+        // create a JButtonCustomizer to customize a button using the String constructor.
         JButtonCustomizer buttonCustomizer = new JButtonCustomizer("Double click to edit this button!");
         // add it to the JCustomizerPane
         customizerPane.addCustomizer(buttonCustomizer, new AbsoluteTableConstraints(300, 100,  210, 50, buttonCustomizer, itl));
+        // create a JButtonCustomizer to customize a JCheckBox
+        JButtonCustomizer checkBoxCustomizer = new JButtonCustomizer(new JCheckBox("Double click to edit this check box!", true));
+        // set the background color to white
+        checkBoxCustomizer.setBackground(Color.WHITE);
+        // add it to the JCustomizerPane
+        customizerPane.addCustomizer(checkBoxCustomizer, new AbsoluteTableConstraints(30, 160,  250, 20, checkBoxCustomizer, itl));
+        // create a JButtonCustomizer to customize a JRadioButton
+        JButtonCustomizer radioButtonCustomizer = new JButtonCustomizer(new JRadioButton("Double click to edit this radio button!", true));
+        // set the background color to white
+        radioButtonCustomizer.setBackground(Color.WHITE);
+        // add it to the JCustomizerPane
+        customizerPane.addCustomizer(radioButtonCustomizer, new AbsoluteTableConstraints(300, 200,  270, 20, radioButtonCustomizer, itl));
         // create a JHtmlCustomizer, which supports inline editing of a text
         JHtmlCustomizer htmlCustomizer = new JHtmlCustomizer();
         // set some HTML text
@@ -57,7 +72,7 @@ public class TextCustomizersSample extends javax.swing.JFrame {
                 "<font color=\"#00FFFF\">colored text!</font><br><br>" +
                 "This is a very long text that shows automatic line wrapping!");
         // add it to the JCustomizerPane
-        customizerPane.addCustomizer(htmlCustomizer, new AbsoluteTableConstraints(150, 200,  270, 150, htmlCustomizer, itl));
+        customizerPane.addCustomizer(htmlCustomizer, new AbsoluteTableConstraints(150, 250,  270, 150, htmlCustomizer, itl));
         setSize(640, 480);
     }
     
--- lib/src/org/softsmithy/lib/swing/JButtonCustomizer.java
+++ lib/src/org/softsmithy/lib/swing/JButtonCustomizer.java
@@ -1,162 +1,87 @@
 /*
-
  *                  Sun Public License Notice
-
  *
-
  * The contents of this file are subject to the Sun Public License
-
  * Version 1.0 (the "License"); you may not use this file except in
-
  * compliance with the License. A copy of the License is available at
-
  * http://www.sun.com/
-
  *
-
  * The Original Code is SoftSmithy Utility Library. The Initial Developer of the
-
  * Original Code is Florian Brunner (Sourceforge.net user: puce). All Rights Reserved.
-
  *
-
  * Contributor(s): .
-
  */
-
-
 
 /*
-
  * JLabelCustomizer.java
-
  *
-
  * Created on 5. September 2002, 17:11
-
  */
-
-
 
 package org.softsmithy.lib.swing;
 
-
-
 import javax.swing.*;
-
 import org.softsmithy.lib.swing.customizer.*;
 
-
-
 /**
-
  *
-
  * @author  puce
-
  */
-
 public class JButtonCustomizer extends AbstractTextCustomizer {
-
   
-
   /** Creates a new instance of JLabelCustomizer */
-
   public JButtonCustomizer() {
-
     this("");
-
   }
-
   
-
   public JButtonCustomizer(AbstractButton button){
-
     super(button);
-
   }
-
   
-
   public JButtonCustomizer(String text){
-
     this(new JButton(text));
     getButton().setBorder(BorderFactory.createRaisedBevelBorder());
   }
 
   
-
   public String getText() {
-
     AbstractButton button = getButton();
-
     return button != null ? button.getText() : "";
-
   }
-
   
-
   public void setText(String text) {
-
     AbstractButton button = getButton();
-
     if (button != null){
-
       button.setText(text);
-
     }
-
   }
-
   
-
   /** Setter for property fComponent.
-
    * @param fComponent New value of property fComponent.
-
    *
-
    */
-
   public void setComponent(JComponent component) {
-
     if (! (component instanceof AbstractButton)){
-
       throw new IllegalArgumentException("comp must be an AbstractButton");
-
     }
-
     super.setComponent(component);
-
   }
-
     protected void setHorizontalAlignmentOnly(HorizontalAlignment alignment) {
-
     AbstractButton button = getButton();
-
     if (button != null){
-
       button.setHorizontalAlignment(alignment.getSwingConstant());
-
     }
-
   }
 
-  
-  public HorizontalAlignment getHorizontalAlignment() {
-
+   public HorizontalAlignment getHorizontalAlignment() {
     AbstractButton button = getButton();
-
     return button != null ? HorizontalAlignment.getHorizontalAlignment(button.getHorizontalAlignment()) : HorizontalAlignment.getHorizontalAlignment(new JButton().getHorizontalAlignment());
-
   }
-  
+ 
   // TODO replace with generic getComponent!?
   private AbstractButton getButton(){
       return (AbstractButton) getComponent();
   }
-
-  
 
 }
 
