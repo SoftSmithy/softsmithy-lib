e04a80576cda57cece52929ebb6ae84cef78b1ec
Florian Brunner <fbrunnerreg@gmx.ch>
2008-07-14 22:24:17 +0000

    - generified

--- lib/src/org/softsmithy/lib/swing/filechooser/FileRootCellRenderer.java
+++ lib/src/org/softsmithy/lib/swing/filechooser/FileRootCellRenderer.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.softsmithy.lib.swing.filechooser;
 
 import java.io.File;
@@ -10,43 +9,27 @@ import java.util.Locale;
 import javax.swing.Icon;
 import javax.swing.filechooser.FileSystemView;
 import org.softsmithy.lib.swing.AbstractCellRenderer;
-import org.softsmithy.lib.swing.HorizontalAlignment;
 
 /**
  *
  * @author puce
  */
-public class FileRootCellRenderer extends AbstractCellRenderer{
+public class FileRootCellRenderer extends AbstractCellRenderer<File> {
 
-    public FileRootCellRenderer() {
-        super(HorizontalAlignment.LEADING);
-    }
-
-    public Object getDisplayValue(Object value, Locale inLocale) {
-        if (value != null){
-            if (! (value instanceof File)){
-                  throw new IllegalArgumentException("value must be an instance of File!");
-            } else {
-                return FileSystemView.getFileSystemView().getSystemDisplayName((File) value);
-            }
+    public Object getDisplayValue(File value, Locale inLocale) {
+        if (value != null) {
+            return FileSystemView.getFileSystemView().getSystemDisplayName(value);
         } else {
             return "";
         }
     }
 
     @Override
-    public Icon getIcon(Object value) {
-        if (value != null){
-            if (! (value instanceof File)){
-                  throw new IllegalArgumentException("value must be an instance of File!");
-            } else {
-                return FileSystemView.getFileSystemView().getSystemIcon((File) value);
-            }
+    public Icon getIcon(File value) {
+        if (value != null) {
+            return FileSystemView.getFileSystemView().getSystemIcon(value);
         } else {
             return null;
         }
     }
-    
-    
-
 }
