0e833a1b274607796d87c811610031d7706fff92
Florian Brunner <fbrunnerreg@gmx.ch>
2008-12-31 11:16:26 +0000

    Added license notice

--- lib/src/org/softsmithy/lib/swing/JFileRootComboBox.java
+++ lib/src/org/softsmithy/lib/swing/JFileRootComboBox.java
@@ -1,6 +1,15 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *                  Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is SoftSmithy Utility Library. The Initial Developer of the
+ * Original Code is Florian Brunner (Sourceforge.net user: puce). All Rights Reserved.
+ *
+ * Contributor(s): .
  */
 
 package org.softsmithy.lib.swing;
