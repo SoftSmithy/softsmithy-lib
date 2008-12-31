e623d80b8fa04dce4bbc381c550d1ebfd48ea64f
Florian Brunner <fbrunnerreg@gmx.ch>
2008-12-31 12:43:16 +0000

    Reformatted

--- lib/src/org/softsmithy/lib/swing/customizer/State.java
+++ lib/src/org/softsmithy/lib/swing/customizer/State.java
@@ -1,130 +1,63 @@
 /*
-
  *                  Sun Public License Notice
-
  *
-
  * The contents of this file are subject to the Sun Public License
-
  * Version 1.0 (the "License"); you may not use this file except in
-
  * compliance with the License. A copy of the License is available at
-
  * http://www.sun.com/
-
  *
-
  * The Original Code is SoftSmithy Utility Library. The Initial Developer of the
-
  * Original Code is Florian Brunner (Sourceforge.net user: puce). All Rights Reserved.
-
  *
-
  * Contributor(s): .
-
  */
-
-
-
 package org.softsmithy.lib.swing.customizer;
 
-
-
 import java.awt.*;
-
 import java.awt.event.*;
-
 import org.softsmithy.lib.swing.*;
-
-
-
-
 
 public interface State {
 
-  
+    void mouseMoved(MouseEvent e);
 
-  void mouseMoved(MouseEvent e);
+    void mouseDragged(MouseEvent e);
 
-  void mouseDragged(MouseEvent e);
+    void applyCursor();
 
-  
+    void focusGained(FocusEvent e);
 
-  void applyCursor();
+    void mousePressed(MouseEvent e);
 
-  
+    void mouseClicked(MouseEvent e);
 
-  void focusGained(FocusEvent e);
+    void focusLost(FocusEvent e);
 
-  
+    JCustomizer getCustomizer();
 
-  void mousePressed(MouseEvent e);
+    void mouseReleased(MouseEvent e);
 
-  
+    void mouseExited(MouseEvent e);
 
-  void mouseClicked(MouseEvent e);
+    void configureCustomizer();
 
-  
+    void unconfigureCustomizer();
 
-  void focusLost(FocusEvent e);
+    void applyBorder();
 
-  
+    void mouseEntered(MouseEvent e);
 
-  JCustomizer getCustomizer();
+    void resetBorder(Color borderColor);
 
-  
+    /** Getter for property active.
+     * @return Value of property active.
+     *
+     */
+    public boolean isActive();
 
-  void mouseReleased(MouseEvent e);
-
-  
-
-  void mouseExited(MouseEvent e);
-
-  
-
-  void configureCustomizer();
-
-  void unconfigureCustomizer();
-
-  
-
-  void applyBorder();
-
-  
-
-  
-
-  void mouseEntered(MouseEvent e);
-
-  
-
-  void resetBorder(Color borderColor);
-
-  
-
-  /** Getter for property active.
-
-   * @return Value of property active.
-
-   *
-
-   */
-
-  public boolean isActive();  
-
-  
-
-  /** Setter for property active.
-
-   * @param active New value of property active.
-
-   *
-
-   */
-
-  public void setActive(boolean active);
-
-  
-
+    /** Setter for property active.
+     * @param active New value of property active.
+     *
+     */
+    public void setActive(boolean active);
 }
-
--- lib/src/org/softsmithy/lib/swing/customizer/StateWrapper.java
+++ lib/src/org/softsmithy/lib/swing/customizer/StateWrapper.java
@@ -1,205 +1,114 @@
 /*
-
  *                  Sun Public License Notice
-
  *
-
  * The contents of this file are subject to the Sun Public License
-
  * Version 1.0 (the "License"); you may not use this file except in
-
  * compliance with the License. A copy of the License is available at
-
  * http://www.sun.com/
-
  *
-
  * The Original Code is SoftSmithy Utility Library. The Initial Developer of the
-
  * Original Code is Florian Brunner (Sourceforge.net user: puce). All Rights Reserved.
-
  *
-
  * Contributor(s): .
-
  */
-
-
-
 package org.softsmithy.lib.swing.customizer;
 
-
-
 import java.awt.*;
-
 import java.awt.event.*;
-
 import org.softsmithy.lib.swing.*;
-
-
 
 public class StateWrapper implements State {
 
-  
-
-  /** Holds value of property state. */
-
-  private State state;
-
-  
-
-  public StateWrapper(State state){
-
-    this.state = state;
-
-  }
-
-  
-
-  public void focusGained(FocusEvent e){
-
-    state.focusGained(e);
-
-  }
-
-  public void focusLost(FocusEvent e){
-
-    state.focusLost(e);
-
-  }
-
-  public void mousePressed(MouseEvent e){
-
-    state.mousePressed(e);
-
-  }
-
-  public void mouseReleased(MouseEvent e){
-
-    state.mouseReleased(e);
-
-  }
-
-  public void mouseClicked(MouseEvent e){
-
-    state.mouseClicked(e);
-
-  }
-
-  public void mouseExited(MouseEvent e){
-
-    state.mouseExited(e);
-
-  }
-
-  public void mouseEntered(MouseEvent e){
-
-    state.mouseEntered(e);
-
-  }
-
-  public void mouseMoved(MouseEvent e){
-
-    state.mouseMoved(e);
-
-  }
-
-  public void mouseDragged(MouseEvent e){
-
-    state.mouseDragged(e);
-
-  }
-
-  public  void configureCustomizer(){
-
-    state.configureCustomizer();
-
-  }
-
-   public  void unconfigureCustomizer(){
-
-    state.unconfigureCustomizer();
-
-  }
-
-  public  void applyBorder(){
-
-    state.applyBorder();
-
-  }
-
-  public  void applyCursor(){
-
-    state.applyCursor();
-
-  }
-
-  public JCustomizer getCustomizer(){
-
-    return state.getCustomizer();
-
-  }
-
-  
-
-  /** Getter for property state.
-
-   * @return Value of property state.
-
-   *
-
-   */
-
-  public State getState() {
-
-    return this.state;
-
-  }
-
-  
-
-  public void resetBorder(Color borderColor) {
-
-    state.resetBorder(borderColor);
-
-  }  
-
-  
-
-  /** Getter for property active.
-
-   * @return Value of property active.
-
-   *
-
-   *
-
-   */
-
-  public boolean isActive() {
-
-    return state.isActive();
-
-  }
-
-  
-
-  /** Setter for property active.
-
-   * @param active New value of property active.
-
-   *
-
-   *
-
-   */
-
-  public void setActive(boolean active) {
-
-    state.setActive(active);
-
-  }
-
-  
-
+    /** Holds value of property state. */
+    private State state;
+
+    public StateWrapper(State state) {
+        this.state = state;
+    }
+
+    public void focusGained(FocusEvent e) {
+        state.focusGained(e);
+    }
+
+    public void focusLost(FocusEvent e) {
+        state.focusLost(e);
+    }
+
+    public void mousePressed(MouseEvent e) {
+        state.mousePressed(e);
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        state.mouseReleased(e);
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        state.mouseClicked(e);
+    }
+
+    public void mouseExited(MouseEvent e) {
+        state.mouseExited(e);
+    }
+
+    public void mouseEntered(MouseEvent e) {
+        state.mouseEntered(e);
+    }
+
+    public void mouseMoved(MouseEvent e) {
+        state.mouseMoved(e);
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        state.mouseDragged(e);
+    }
+
+    public void configureCustomizer() {
+        state.configureCustomizer();
+    }
+
+    public void unconfigureCustomizer() {
+        state.unconfigureCustomizer();
+    }
+
+    public void applyBorder() {
+        state.applyBorder();
+    }
+
+    public void applyCursor() {
+        state.applyCursor();
+    }
+
+    public JCustomizer getCustomizer() {
+        return state.getCustomizer();
+    }
+
+    /** Getter for property state.
+     * @return Value of property state.
+     *
+     */
+    public State getState() {
+        return this.state;
+    }
+
+    public void resetBorder(Color borderColor) {
+        state.resetBorder(borderColor);
+    }
+
+    /** Getter for property active.
+     * @return Value of property active.
+     *
+     *
+     */
+    public boolean isActive() {
+        return state.isActive();
+    }
+
+    /** Setter for property active.
+     * @param active New value of property active.
+     *
+     *
+     */
+    public void setActive(boolean active) {
+        state.setActive(active);
+    }
 }
