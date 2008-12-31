caed6ff0d07d26c1d4961ccacfb8445df6692a92
Florian Brunner <fbrunnerreg@gmx.ch>
2008-12-31 12:09:59 +0000

    Reformatted

--- lib/src/org/softsmithy/lib/swing/LongCellEditor.java
+++ lib/src/org/softsmithy/lib/swing/LongCellEditor.java
@@ -1,130 +1,61 @@
 /*
-
  *                  Sun Public License Notice
-
  *
-
  * The contents of this file are subject to the Sun Public License
-
  * Version 1.0 (the "License"); you may not use this file except in
-
  * compliance with the License. A copy of the License is available at
-
  * http://www.sun.com/
-
  *
-
  * The Original Code is SoftSmithy Utility Library. The Initial Developer of the
-
  * Original Code is Florian Brunner (Sourceforge.net user: puce). All Rights Reserved.
-
  *
-
  * Contributor(s): .
-
  */
-
-
 
 /*
-
  * IntegerCellEditor.java
-
  *
-
  * Created on 7. Oktober 2002, 16:21
-
  */
-
-
-
 package org.softsmithy.lib.swing;
-
-
 
 import java.util.*;
 
-
-
 /**
-
  *
-
  * @author  puce
-
  */
-
 public class LongCellEditor extends FormattedCellEditor {
 
-  
+    /** Creates a new instance of IntegerCellEditor */
+    public LongCellEditor(Locale locale) {
+        super(new JLongField(locale));
+    }
 
-  /** Creates a new instance of IntegerCellEditor */
+    public LongCellEditor(long minValue, long maxValue, Locale locale) {
+        super(new JLongField(minValue, maxValue, locale));
+    }
 
-  public LongCellEditor(Locale locale) {
+    public JLongField getLongField() {
+        return (JLongField) getFormattedTextField();
+    }
 
-    super(new JLongField(locale));
+    /** Returns the value contained in the editor.
+     * @return the value contained in the editor
+     *
+     *
+     */
+    //  public Object getCellEditorValue() {
+    //    System.out.println(super.getCellEditorValue().getClass());
+    //    System.out.println(super.getCellEditorValue());
+    //    Number number = (Number) super.getCellEditorValue(); //sometimes an Integer is returned, sometimes a Long???
+    //    return new Integer(number.intValue());
+    //  }
+    protected void setValue(Object value) {
+        getLongField().setLongValue(((Long) value).longValue());
+    }
 
-  }
-
-  
-
-  public LongCellEditor(long minValue, long maxValue, Locale locale) {
-
-    super(new JLongField(minValue, maxValue, locale));
-
-  }
-
-  
-
-  public JLongField getLongField(){
-
-    return (JLongField) getFormattedTextField();
-
-  }
-
-  
-
-  /** Returns the value contained in the editor.
-
-   * @return the value contained in the editor
-
-   *
-
-   *
-
-   */
-
-  //  public Object getCellEditorValue() {
-
-  //    System.out.println(super.getCellEditorValue().getClass());
-
-  //    System.out.println(super.getCellEditorValue());
-
-  //    Number number = (Number) super.getCellEditorValue(); //sometimes an Integer is returned, sometimes a Long???
-
-  //    return new Integer(number.intValue());
-
-  //  }
-
-  
-
-  protected void setValue(Object value) {
-
-    getLongField().setLongValue(((Long) value).longValue());
-
-  }
-
-  
-
-  protected Object getValue() {
-
-    return new Long(getLongField().getLongValue());
-
-  }
-
-  
-
+    protected Object getValue() {
+        return new Long(getLongField().getLongValue());
+    }
 }
-
-
-
--- lib/src/org/softsmithy/lib/swing/NumberListCellRenderer.java
+++ lib/src/org/softsmithy/lib/swing/NumberListCellRenderer.java
@@ -1,74 +1,35 @@
 /*
-
  *                  Sun Public License Notice
-
  *
-
  * The contents of this file are subject to the Sun Public License
-
  * Version 1.0 (the "License"); you may not use this file except in
-
  * compliance with the License. A copy of the License is available at
-
  * http://www.sun.com/
-
  *
-
  * The Original Code is SoftSmithy Utility Library. The Initial Developer of the
-
  * Original Code is Florian Brunner (Sourceforge.net user: puce). All Rights Reserved.
-
  *
-
  * Contributor(s): .
-
  */
-
-
 
 /*
-
  * NumberCellRenderer.java
-
  *
-
  * Created on 1. Oktober 2002, 11:46
-
  */
-
-
-
 package org.softsmithy.lib.swing;
 
-
-
 import javax.swing.*;
-
 import javax.swing.plaf.basic.*;
 
-
-
 /**
-
  *
-
  * @author  puce
-
  */
+class NumberListCellRenderer extends DefaultListCellRenderer { //BasicComboBoxRenderer {
 
-class NumberListCellRenderer extends DefaultListCellRenderer{ //BasicComboBoxRenderer {
-
-  
-
-  /** Creates a new instance of NumberCellRenderer */
-
-  public NumberListCellRenderer() {
-
-    setHorizontalAlignment(SwingConstants.RIGHT);
-
-  }
-
-  
-
+    /** Creates a new instance of NumberCellRenderer */
+    public NumberListCellRenderer() {
+        setHorizontalAlignment(SwingConstants.RIGHT);
+    }
 }
-
