3367b409b122aab009cb948fc6188087a02eab1e
Florian Brunner <fbrunnerreg@gmx.ch>
2008-12-31 10:38:41 +0000

    Reformatted

--- lib/src/org/softsmithy/lib/swing/ByteCellEditor.java
+++ lib/src/org/softsmithy/lib/swing/ByteCellEditor.java
@@ -1,128 +1,61 @@
 /*
-
  *                  Sun Public License Notice
-
  *
-
  * The contents of this file are subject to the Sun Public License
-
  * Version 1.0 (the "License"); you may not use this file except in
-
  * compliance with the License. A copy of the License is available at
-
  * http://www.sun.com/
-
  *
-
  * The Original Code is SoftSmithy Utility Library. The Initial Developer of the
-
  * Original Code is Florian Brunner (Sourceforge.net user: puce). All Rights Reserved.
-
  *
-
  * Contributor(s): .
-
  */
-
-
 
 /*
-
  * IntegerCellEditor.java
-
  *
-
  * Created on 7. Oktober 2002, 16:21
-
  */
-
-
-
 package org.softsmithy.lib.swing;
-
-
 
 import java.util.*;
 
-
-
 /**
-
  *
-
  * @author  puce
-
  */
-
 public class ByteCellEditor extends FormattedCellEditor {
 
-  
+    /** Creates a new instance of IntegerCellEditor */
+    public ByteCellEditor(Locale locale) {
+        super(new JByteField(locale));
+    }
 
-  /** Creates a new instance of IntegerCellEditor */
+    public ByteCellEditor(byte minValue, byte maxValue, Locale locale) {
+        super(new JByteField(minValue, maxValue, locale));
+    }
 
-  public ByteCellEditor(Locale locale) {
+    public JByteField getByteField() {
+        return (JByteField) getFormattedTextField();
+    }
 
-    super(new JByteField(locale));
-
-  }
-
-  
-
-  public ByteCellEditor(byte minValue, byte maxValue, Locale locale) {
-
-    super(new JByteField(minValue, maxValue, locale));
-
-  }
-
-  
-
-  public JByteField getByteField(){
-
-    return (JByteField) getFormattedTextField();
-
-  }
-
-  
-
-  /** Returns the value contained in the editor.
-
-   * @return the value contained in the editor
-
-   *
-
-   *
-
-   */
-
+    /** Returns the value contained in the editor.
+     * @return the value contained in the editor
+     *
+     *
+     */
 //  public Object getCellEditorValue() {
-
 //    System.out.println(super.getCellEditorValue().getClass());
-
 //    System.out.println(super.getCellEditorValue());
-
 //    Number number = (Number) super.getCellEditorValue(); //sometimes an Integer is returned, sometimes a Long???
-
 //    return new Integer(number.intValue());
-
 //  }
+    protected void setValue(Object value) {
+        getByteField().setByteValue(((Byte) value).byteValue());
+    }
 
-  
-
-  protected void setValue(Object value) {
-
-    getByteField().setByteValue(((Byte) value).byteValue());
-
-  }
-
-  
-
-  protected Object getValue() {
-
-    return new Byte(getByteField().getByteValue());
-
-  }
-
-  
-
+    protected Object getValue() {
+        return new Byte(getByteField().getByteValue());
+    }
 }
-
