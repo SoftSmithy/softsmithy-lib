e17f82c3b74dc2e5f1389aa6ff666e24e3cd7832
Florian Brunner <fbrunnerreg@gmx.ch>
2008-12-31 12:03:22 +0000

    Reformatted

--- lib/src/org/softsmithy/lib/swing/LocalizedFloatCellEditor.java
+++ lib/src/org/softsmithy/lib/swing/LocalizedFloatCellEditor.java
@@ -1,128 +1,61 @@
 /*
-
  *                  Sun Public License Notice
-
  *
-
  * The contents of this file are subject to the Sun Public License
-
  * Version 1.0 (the "License"); you may not use this file except in
-
  * compliance with the License. A copy of the License is available at
-
  * http://www.sun.com/
-
  *
-
  * The Original Code is SoftSmithy Utility Library. The Initial Developer of the
-
  * Original Code is Florian Brunner (Sourceforge.net user: puce). All Rights Reserved.
-
  *
-
  * Contributor(s): .
-
  */
-
-
 
 /*
-
  * LocalizedFloatCellEditor.java
-
  *
-
  * Created on 7. Oktober 2002, 16:21
-
  */
-
-
-
 package org.softsmithy.lib.swing;
-
-
 
 import java.util.*;
 
-
-
 /**
-
  *
-
  * @author  puce
-
  */
-
 public class LocalizedFloatCellEditor extends FormattedCellEditor {
 
-  
+    /** Creates a new instance of LocalizedFloatCellEditor */
+    public LocalizedFloatCellEditor(Locale locale) {
+        super(new JLocalizedFloatField(locale));
+    }
 
-  /** Creates a new instance of LocalizedFloatCellEditor */
+    public LocalizedFloatCellEditor(float minValue, float maxValue, Locale locale) {
+        super(new JLocalizedFloatField(minValue, maxValue, locale));
+    }
 
-  public LocalizedFloatCellEditor(Locale locale) {
+    public JLocalizedFloatField getLocalizedFloatField() {
+        return (JLocalizedFloatField) getFormattedTextField();
+    }
 
-    super(new JLocalizedFloatField(locale));
-
-  }
-
-  
-
-  public LocalizedFloatCellEditor(float minValue, float maxValue, Locale locale) {
-
-    super(new JLocalizedFloatField(minValue, maxValue, locale));
-
-  }
-
-  
-
-  public JLocalizedFloatField getLocalizedFloatField(){
-
-    return (JLocalizedFloatField) getFormattedTextField();
-
-  }
-
-  
-
-  /** Returns the value contained in the editor.
-
-   * @return the value contained in the editor
-
-   *
-
-   *
-
-   */
-
+    /** Returns the value contained in the editor.
+     * @return the value contained in the editor
+     *
+     *
+     */
 //  public Object getCellEditorValue() {
-
 //    System.out.println(super.getCellEditorValue().getClass());
-
 //    System.out.println(super.getCellEditorValue());
-
 //    Number number = (Number) super.getCellEditorValue(); //sometimes an Integer is returned, sometimes a Long???
-
 //    return new Intteger(number.intValue());
-
 //  }
+    protected void setValue(Object value) {
+        getLocalizedFloatField().setFloatValue(((Float) value).floatValue());
+    }
 
-  
-
-  protected void setValue(Object value) {
-
-    getLocalizedFloatField().setFloatValue(((Float) value).floatValue());
-
-  }
-
-  
-
-  protected Object getValue() {
-
-    return new Float(getLocalizedFloatField().getFloatValue());
-
-  }
-
-  
-
+    protected Object getValue() {
+        return new Float(getLocalizedFloatField().getFloatValue());
+    }
 }
-
