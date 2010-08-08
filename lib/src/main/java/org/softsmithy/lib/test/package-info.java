2bcf77616c7cd8ade111d15ff93d32bbf0122cfd
Florian Brunner <fbrunnerreg@gmx.ch>
2010-08-08 20:41:10 +0000

    package-info.java

---
+++ lib/src/main/java/org/softsmithy/lib/test/package-info.java
@@ -0,0 +1,39 @@
+/*
+ *                  Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is SoftSmithy Utility Library. The Initial Developer of the
+ * Original Code is Florian Brunner (Sourceforge.net user: puce). All Rights Reserved.
+ *
+ * Contributor(s): .
+ */
+/**
+ *
+ * <!-- ##### THIS IS THE TEMPLATE FOR THE PACKAGE DOC COMMENTS. #####
+ * ##### TYPE YOUR PACKAGE COMMENTS HERE.  BEGIN WITH A     #####
+ * ##### ONE-SENTENCE SUMMARY STARTING WITH A VERB LIKE:    ##### -->
+ * Provides classes and interfaces for testing purposes that should not be relied
+ * upon outside this library!
+ *
+ *
+ * <h2>Package Specification</h2>
+ *
+ * <!-- ##### FILL IN ANY SPECS NEEDED BY JAVA COMPATIBILITY KIT ##### -->
+ * <ul>
+ *  <li><a href=""><!-- ##### REFER TO ANY FRAMEMAKER SPECIFICATION HERE ##### --></a>
+ * </ul>
+ *
+ * <h2>Related Documentation</h2>
+ *
+ * For overviews, tutorials, examples, guides, and tool documentation, please see:
+ * <ul>
+ *  <li><a href=""><!-- ##### REFER TO NON-SPEC DOCUMENTATION HERE ##### --></a>
+ * </ul>
+ *
+ * <!-- Put @see and @since tags down here. -->
+ */
+package org.softsmithy.lib.test;
