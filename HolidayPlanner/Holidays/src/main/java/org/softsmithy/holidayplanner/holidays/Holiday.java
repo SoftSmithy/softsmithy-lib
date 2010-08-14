d34be553fa48f13113b74ab1d79349ea160f53e6
Florian Brunner <fbrunnerreg@gmx.ch>
2010-08-14 00:04:06 +0000

    HolidayPlanner
    - Holiday Entity refactored
    - HolidaysOverviewTopComponent added

--- HolidayPlanner/Holidays/src/main/java/org/softsmithy/holidayplanner/holidays/Holiday.java
+++ HolidayPlanner/Holidays/src/main/java/org/softsmithy/holidayplanner/holidays/Holiday.java
@@ -5,54 +5,23 @@
 
 package org.softsmithy.holidayplanner.holidays;
 
-import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.Temporal;
+import org.softsmithy.lib.persistence.AbstractEntity;
 
 /**
  *
  * @author Florian
  */
 @Entity
-public class Holiday implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+public class Holiday extends AbstractEntity {
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Holiday)) {
-            return false;
-        }
-        Holiday other = (Holiday) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "org.softsmithy.holidayplanner.holidays.Holiday[id=" + id + "]";
-    }
+    private String title;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date startDate;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date endDate;
+   
 
 }
