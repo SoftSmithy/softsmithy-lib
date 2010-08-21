abb0db8d7cbc1f28be16052cde2faa3fe808c266
Florian Brunner <fbrunnerreg@gmx.ch>
2010-08-21 17:29:08 +0000

    lib-samples: Layout: use Java SE 6 generated code

--- lib/docs/samples/src/main/java/samples/swing/component/FileRootSelectionSample.java
+++ lib/docs/samples/src/main/java/samples/swing/component/FileRootSelectionSample.java
@@ -1,8 +1,17 @@
 /*
- * LocaleSelectionSample.java
+ *                  Sun Public License Notice
  *
- * Created on 11. August 2007, 18:48
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is SoftSmithy Utility Library. The Initial Developer of the
+ * Original Code is Florian Brunner (Sourceforge.net user: puce). All Rights Reserved.
+ *
+ * Contributor(s): .
  */
+
 package samples.swing.component;
 
 /**
@@ -72,6 +81,7 @@ private void fileRootListValueChanged(javax.swing.event.ListSelectionEvent evt) 
      */
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new FileRootSelectionSample().setVisible(true);
             }
