0fce4c99c93e0051b9d9ddb0ab0235e06eb82388
Florian Brunner <fbrunnerreg@gmx.ch>
2010-10-18 21:17:34 +0000

    - GenericCollationKey added - LocalizedCollator added - Unit tests added

---
+++ lib/lib-core/src/main/java/org/softsmithy/lib/text/LocalizedCollator.java
@@ -0,0 +1,77 @@
+/*
+ *         COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Notice
+ *
+ * The contents of this file are subject to the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL)
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.opensource.org/licenses/cddl1.txt
+ *
+ * The Original Code is SoftSmithy Utility Library. The Initial Developer of the
+ * Original Code is Florian Brunner (Sourceforge.net user: puce). All Rights Reserved.
+ *
+ * Contributor(s): .
+ */
+package org.softsmithy.lib.text;
+
+import java.text.Collator;
+import java.util.Locale;
+
+/**
+ *
+ * @author puce
+ */
+public class LocalizedCollator {
+
+    private final Locale locale;
+    private final Collator collator;
+
+    public LocalizedCollator(Locale locale) {
+        this.locale = locale;
+        this.collator = Collator.getInstance(locale);
+    }
+
+    /**
+     * @return the locale
+     */
+    public Locale getLocale() {
+        return locale;
+    }
+
+    /**
+     * @return the collator
+     */
+    public Collator getCollator() {
+        return collator;
+    }
+
+    /**
+     * @see #getCollationKey(java.text.Collator, java.lang.Object, org.softsmithy.lib.text.Localizer, java.util.Locale)
+     * @see LocalizableComparator
+     * @see LocalizerComparator
+     * @param <T>
+     * @param source
+     * @return
+     */
+    public <T extends Localizable> GenericCollationKey<T> getCollationKey(
+            T source) {
+        return new GenericCollationKey<T>(source,
+                getCollator().getCollationKey(source.getDisplayString(
+                getLocale())));
+    }
+
+    /**
+     * @see #getCollationKey(java.text.Collator, org.softsmithy.lib.text.Localizable, java.util.Locale)
+     * @see LocalizerComparator
+     * @see LocalizableComparator
+     * @param <T>
+     * @param source
+     * @param localizer
+     * @return
+     */
+    public <T> GenericCollationKey<T> getCollationKey(T source,
+            Localizer<? super T> localizer) {
+        return new GenericCollationKey<T>(source,
+                getCollator().getCollationKey(localizer.getDisplayString(source,
+                getLocale())));
+    }
+}
