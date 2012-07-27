a7507bb3a0926095acb0ee6657ffa052f13a8644
Florian Brunner <fbrunnerreg@gmx.ch>
2012-07-27 10:44:05 +0200

    - samples: GetInstallationDirSample added

---
+++ softsmithy-lib-samples/src/main/java/samples/nio/file/GetInstallationDirSample.java
@@ -0,0 +1,47 @@
+/*
+ *         COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Notice
+ *
+ * The contents of this file are subject to the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL)
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.opensource.org/licenses/cddl1.txt
+ *
+ * The Original Code is SoftSmithy Utility Library. The Initial Developer of the
+ * Original Code is Florian Brunner (Sourceforge.net user: puce). All Rights Reserved.
+ *
+ * Contributor(s): .
+ */
+package samples.nio.file;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.softsmithy.lib.nio.file.JarFiles;
+
+/**
+ *
+ * @author puce
+ */
+public class GetInstallationDirSample {
+
+    /**
+     * java
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        GetInstallationDirSample getInstallationDirSample = new GetInstallationDirSample();
+        getInstallationDirSample.printInstallDirPath();
+    }
+
+    public void printInstallDirPath() throws IOException, URISyntaxException {
+        URI jarURI = JarFiles.getJarURI(GetInstallationDirSample.class);
+        Path jarPath = Paths.get(jarURI);
+        Path installDirPath = jarPath.getParent().getParent().getParent().getParent();
+        
+        System.out.println(installDirPath);
+    }
+}
