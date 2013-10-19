b1e48105cc9722c471260f29423395df742ddf66
Florian Brunner <fbrunnerreg@gmx.ch>
2013-10-19 22:22:15 +0200

    Moved java.time utility classes from Dombler Commons to SoftSmithy.

---
+++ softsmithy-lib-time/src/main/java/org/softsmithy/lib/time/WeekOfYearUtils.java
@@ -0,0 +1,30 @@
+/*
+ *         COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Notice
+ *
+ * The contents of this file are subject to the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL)
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.opensource.org/licenses/cddl1.txt
+ *
+ * The Original Code is SoftSmithy Utility Library. The Initial Developer of the
+ * Original Code is Florian Brunner (Sourceforge.net user: puce). All Rights Reserved.
+ *
+ * Contributor(s): .
+ */
+package org.softsmithy.lib.time;
+
+//import java.time.calendar.DateTimeFieldRule;
+
+/**
+ * 
+ * @author puce
+ */
+class WeekOfYearUtils {
+
+    private WeekOfYearUtils() {
+    }
+    
+//    public static int getWeeksOfYear(LocalDate startDate, LocalDate endDate, DateTimeFieldRule<Integer> weekOfYearRule){
+//        return weekOfYearRule.getInt(endDate) - weekOfYearRule.getInt(startDate) + 1;
+//    }
+}
