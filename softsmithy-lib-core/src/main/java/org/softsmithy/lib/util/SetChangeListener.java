f4de39932abd54055dc05c30a98dc23e0a2c8d40
Florian Brunner <fbrunnerreg@gmx.ch>
2016-09-05 01:15:58 +0200

    #111 File Type Editor Registration - license headers added

--- softsmithy-lib-core/src/main/java/org/softsmithy/lib/util/SetChangeListener.java
+++ softsmithy-lib-core/src/main/java/org/softsmithy/lib/util/SetChangeListener.java
@@ -1,7 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *         COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Notice
+ *
+ * The contents of this file are subject to the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL)
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.opensource.org/licenses/cddl1.txt
+ *
+ * The Original Code is SoftSmithy Utility Library. The Initial Developer of the
+ * Original Code is Florian Brunner (GitHub user: puce77). All Rights Reserved.
+ *
+ * Contributor(s): .
  */
 package org.softsmithy.lib.util;
 
