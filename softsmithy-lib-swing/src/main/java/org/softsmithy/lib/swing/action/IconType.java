3b8a5c35a0525af3b054beaf69344ed06fd57e01
Florian Brunner <fbrunnerreg@gmx.ch>
2016-09-17 01:39:29 +0200

    #16 Replace TypesafeEnum with enum - migrated IconType to standard enum

--- softsmithy-lib-swing/src/main/java/org/softsmithy/lib/swing/action/IconType.java
+++ softsmithy-lib-swing/src/main/java/org/softsmithy/lib/swing/action/IconType.java
@@ -11,48 +11,27 @@
  *
  * Contributor(s): .
  */
-
 package org.softsmithy.lib.swing.action;
 
-import java.util.*;
-import org.softsmithy.lib.util.TypesafeEnum;
-
 /**
- * A typesafe enum class. Defines some icon types. <br>
+ * This enum defines some icon types. <br>
  * Note: This class may change package in a future version.
  *
- * @author    Florian Brunner
+ * @author Florian Brunner
  */
+public enum IconType {
 
-public class IconType extends TypesafeEnum {
+    /**
+     * Large icon type. By convention large icons have 24x24 pixel.
+     */
+    LARGE_ICON,
+    /**
+     * Small icon type. By convention small icons have 16x16 pixel.
+     */
+    SMALL_ICON,
+    /**
+     * No icon type.
+     */
+    NO_ICON;
 
-  /**
-   * Creates a new icon type.
-   *
-   * @param name  the name of this icon type
-   */
-  private IconType(String name) {
-    super(name);
-  }
-
-  /**
-   * Large icon type. By convention large icons have 24x24 pixel.
-   */
-  public final static IconType LARGE_ICON = new IconType("largeIcon");
-  /**
-   * Small icon type. By convention small icons have 16x16 pixel.
-   */
-  public final static IconType SMALL_ICON = new IconType("smallIcon");
-  /**
-   * No icon type.
-   */
-  public final static IconType NO_ICON = new IconType("noIcon");
-
-  private final static IconType[] PRIVATE_VALUES = {LARGE_ICON, SMALL_ICON, NO_ICON};
-
-  /**
-   * A list of all defined icon types.
-   */
-  public final static List VALUES =
-      Collections.unmodifiableList(Arrays.asList(PRIVATE_VALUES));
 }
