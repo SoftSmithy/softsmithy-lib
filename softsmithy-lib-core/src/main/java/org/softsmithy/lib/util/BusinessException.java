f9d4c466aef5894ada0596e8f84ee0992f6c958c
Florian Brunner <fbrunnerreg@gmx.ch>
2018-05-27 21:38:02 +0200

    BusinessException added

---
+++ softsmithy-lib-core/src/main/java/org/softsmithy/lib/util/BusinessException.java
@@ -0,0 +1,43 @@
+/*
+ *         COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Notice
+ *
+ * The contents of this file are subject to the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL)
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.opensource.org/licenses/cddl1.txt
+ *
+ * The Original Code is SoftSmithy Utility Library. The Initial Developer of the
+ * Original Code is Florian Brunner (GitHub user: puce77). All Rights Reserved.
+ *
+ * Contributor(s): .
+ */
+package org.softsmithy.lib.util;
+
+/**
+ *
+ * @author puce
+ */
+public class BusinessException extends Exception {
+
+    private static final long serialVersionUID = -5277539401535691349L;
+
+    public BusinessException() {
+    }
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BusinessException(Throwable cause) {
+        super(cause);
+    }
+
+    public BusinessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
