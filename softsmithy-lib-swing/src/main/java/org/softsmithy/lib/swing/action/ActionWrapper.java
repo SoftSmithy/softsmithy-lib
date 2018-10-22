5161ccc7b27139ddfe1157421cb1e7f61fc4a66c
Florian Brunner <fbrunnerreg@gmx.ch>
2018-10-22 23:36:35 +0200

    - Clean-ups

--- softsmithy-lib-swing/src/main/java/org/softsmithy/lib/swing/action/ActionWrapper.java
+++ softsmithy-lib-swing/src/main/java/org/softsmithy/lib/swing/action/ActionWrapper.java
@@ -11,35 +11,48 @@
  *
  * Contributor(s): .
  */
-
 package org.softsmithy.lib.swing.action;
 
 import java.beans.*;
 import javax.swing.*;
 import org.softsmithy.lib.awt.event.*;
 
-public class ActionWrapper extends ActionListenerWrapper implements Action{
-  private Action action;
-  public ActionWrapper(Action action){
-    super(action);
-    this.action = action;
-  }
-  public java.lang.Object getValue(java.lang.String string){
-    return action.getValue(string);
-  }
-  public void addPropertyChangeListener(PropertyChangeListener propertyChangeListener){
-    action.addPropertyChangeListener(propertyChangeListener);
-  }
-  public void removePropertyChangeListener(PropertyChangeListener propertyChangeListener){
-    action.removePropertyChangeListener(propertyChangeListener);
-  }
-  public void putValue(java.lang.String string, java.lang.Object object){
-    action.putValue(string, object);
-  }
-  public void setEnabled(boolean isEnabled){
-    action.setEnabled(isEnabled);
-  }
-  public boolean isEnabled(){
-    return action.isEnabled();
-  }
+public class ActionWrapper extends ActionListenerWrapper implements Action {
+
+    private final Action action;
+
+    public ActionWrapper(Action action) {
+        super(action);
+        this.action = action;
+    }
+
+    @Override
+    public Object getValue(java.lang.String string) {
+        return action.getValue(string);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener propertyChangeListener) {
+        action.addPropertyChangeListener(propertyChangeListener);
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener propertyChangeListener) {
+        action.removePropertyChangeListener(propertyChangeListener);
+    }
+
+    @Override
+    public void putValue(java.lang.String string, java.lang.Object object) {
+        action.putValue(string, object);
+    }
+
+    @Override
+    public void setEnabled(boolean isEnabled) {
+        action.setEnabled(isEnabled);
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return action.isEnabled();
+    }
 }
