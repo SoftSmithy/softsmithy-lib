76def1903f7b64973b227dc20cf722a626fb3281
Florian Brunner <fbrunnerreg@gmx.ch>
2019-11-10 18:44:49 +0100

    API clean-up

--- softsmithy-lib-compiler/src/main/java/org/softsmithy/lib/lang/model/type/ModelTypeUtils.java
+++ softsmithy-lib-compiler/src/main/java/org/softsmithy/lib/lang/model/type/ModelTypeUtils.java
@@ -13,7 +13,13 @@
  */
 package org.softsmithy.lib.lang.model.type;
 
+import java.lang.annotation.Annotation;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.type.MirroredTypeException;
 import javax.lang.model.type.TypeMirror;
 
@@ -41,5 +47,74 @@ public final class ModelTypeUtils {
             typeMirror = mte.getTypeMirror();
         }
         return typeMirror;
+    }
+
+    /**
+     * Gets the list {@link TypeMirror}s which represent the values of an annotation member of type {@link Class}-array.
+     *
+     * <p>
+     * Given:
+     * <p>
+     * {@code @Foo(barClasses={Bar1.class, Bar2.class})}
+     * <p>
+     * then:
+     * <p>
+     * {@code
+     *  List<String> classNames =  getTypeMirrorsOfClassArrayAnnotationValue(annotationMirrors, Foo.class, "barClasses").stream()
+     *          .map(TypeMirror::toString)
+     *          .collect(Collectors.toList());
+     * }
+     * <p>
+     * returns:
+     * <p>
+     * ["package1.Bar1", "package2.Bar2"]
+     *
+     * @param annotationMirrors the list of annotationMirrors including an annotationMirror for the annotation in question
+     * @param annotationClass the annotation class
+     * @param memberName the name of the annotation member of type {@link Class}-array
+     * @return the class names of an annotation member of type {@link Class}-array.
+     */
+    public static List<TypeMirror> getTypeMirrorsOfClassArrayAnnotationValue(List<? extends AnnotationMirror> annotationMirrors, Class<? extends Annotation> annotationClass, String memberName) {
+        return annotationMirrors.stream()
+                .filter(annotationMirror -> represents(annotationMirror, annotationClass))
+                .map(AnnotationMirror::getElementValues)
+                .flatMap(map -> map.entrySet().stream())
+                .filter(elementValueEntry -> elementValueEntry.getKey().getSimpleName().contentEquals(memberName))
+                .map(Map.Entry::getValue)
+                .map(ModelTypeUtils::getArrayAnnotationValues) // expected value type is an array -> List<? extends AnnotationValue>
+                .flatMap(List::stream)
+                .map(ModelTypeUtils::getClassAnnotationValue) // the expected type of the array is Class -> TypeMirror
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Checks the annotationMirror represents an "instance" of the given annotation.
+     *
+     * @param annotationMirror
+     * @param annotationClass
+     * @return
+     */
+    private static boolean represents(AnnotationMirror annotationMirror, Class<? extends Annotation> annotationClass) {
+        return annotationMirror.getAnnotationType().toString().equals(annotationClass.getName());
+    }
+
+    /**
+     * Gets the values of an annotation member of type array.
+     *
+     * @param annotationValue
+     * @return
+     */
+    private static List<? extends AnnotationValue> getArrayAnnotationValues(AnnotationValue annotationValue) {
+        return (List<? extends AnnotationValue>) annotationValue.getValue();
+    }
+
+    /**
+     * Gets the value of an anntation value, which is expected to represent a Class type -> TypeMirror.
+     *
+     * @param annotationValue
+     * @return
+     */
+    private static TypeMirror getClassAnnotationValue(AnnotationValue annotationValue) {
+        return (TypeMirror) annotationValue.getValue();
     }
 }
