75dd3b80c8cf1f1c673b39e7020b398fc36a68c1
Florian Brunner <fbrunnerreg@gmx.ch>
2020-01-14 22:35:32 +0100

    wagon-git commit

---
+++ devlib/3.0-SNAPSHOT/docs/site/softsmithy-devlib-persistence/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"org.softsmithy.devlib.persistence","l":"org.softsmithy.devlib.persistence"}]
---
+++ devlib/3.0-SNAPSHOT/docs/site/softsmithy-devlib-persistence/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"org.softsmithy.devlib.persistence","l":"AbstractDbInitializer"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.softsmithy.devlib.persistence","l":"DbInitializer"}]
