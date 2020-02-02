59002ce993b8c844df3013631eec4dc3c0f61e6a
Florian Brunner <fbrunnerreg@gmx.ch>
2020-02-02 22:13:05 +0100

    #172 Java 11: make sure Drombler FX works with Java SE 11 - Javadoc - clean-up

--- softsmithy-lib-core/src/main/java/org/softsmithy/lib/util/Resources.java
+++ softsmithy-lib-core/src/main/java/org/softsmithy/lib/util/Resources.java
@@ -14,30 +14,59 @@
 package org.softsmithy.lib.util;
 
 /**
+ * Utility class to work with resources in modular (Java 11+) applications.
  *
  * @version 3.0
  * @author puce
  */
 public final class Resources {
     public static final char RESOURCE_PATH_DELIMITER = '/';
+    private static final char PACKAGE_NAME_DELIMITER = '.';
 
     private Resources() {
     }
 
+    /**
+     * Converts a package name to module resource path.
+     *
+     * @param typePackage the package
+     * @return the module resource path for the provided package.
+     * @see #toModulePackageResourcePath(java.lang.String)
+     * @see Module#getResourceAsStream(java.lang.String)
+     */
     public static final String toModulePackageResourcePath(Package typePackage) {
         return toModulePackageResourcePath(typePackage.getName());
     }
 
+    /**
+     * Converts a package name to module resource path.
+     *
+     * @param packageName the package name
+     * @return the module resource path for the provided package.
+     * @see Module#getResourceAsStream(java.lang.String)
+     */
     public static final String toModulePackageResourcePath(String packageName) {
-        return packageName.replace('.', RESOURCE_PATH_DELIMITER);
+        return packageName.replace(PACKAGE_NAME_DELIMITER, RESOURCE_PATH_DELIMITER);
     }
 
-
+    /**
+     * Gets the converted package name for the provided module resource path.
+     *
+     * @param moduleResourcePath the module resource path
+     * @return the converted package name for the provided module resource path
+     */
     public static final String getPackageName(String moduleResourcePath) {
         int lastIndex = moduleResourcePath.lastIndexOf(RESOURCE_PATH_DELIMITER);
-        return (lastIndex != -1) ? moduleResourcePath.substring(0, lastIndex).replace(RESOURCE_PATH_DELIMITER, '.') : "";
+        return (lastIndex != -1) ? moduleResourcePath.substring(0, lastIndex).replace(RESOURCE_PATH_DELIMITER, PACKAGE_NAME_DELIMITER) : "";
     }
 
+    /**
+     * Checks if the package of the resource is open to the accessing class. If it is not open a {@link ResourceAccessException} gets thrown.
+     *
+     * @param moduleResourcePath the module resource path
+     * @param owningModule the owning module of the resource
+     * @param accessingClass the class accessing the resource
+     */
     public static final void checkPackageIsOpen(String moduleResourcePath, Module owningModule, Class<?> accessingClass) {
         if (owningModule.isNamed() && !owningModule.isOpen(getPackageName(moduleResourcePath), accessingClass.getModule())) {
             throw new ResourceAccessException(moduleResourcePath, owningModule, accessingClass);
